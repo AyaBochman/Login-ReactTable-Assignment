e6b97754bdaa69c663593b87108c685b10d95cc0
Aya Bochman <aya@Ayas-MacBook-Pro.local>
2021-06-23 14:52:56 +0200

    added isLoggenIn to statge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 
 function App() {
+  const { isLoggedIn } = useSelector((state) => ({
+    isLoggedIn: state.isLoggedIn,
+  }));
+
   return (
     <div className="App">
       <h1>LOGIN</h1>
-      <LoginForm />
+      <BrowserRouter>
+        <Switch>
+          {/* <Route path="/">
+            <LoginForm />
+          </Route> */}
+          <Route exact path="/">
+            {isLoggedIn ? <Redirect to="/info" /> : <LoginForm />}
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
